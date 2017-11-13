68fd0f8bba505a108cb921b0b8b57108331a86c9
eifel1390 <maksimturkin50@gmail.com>
2017-11-13 10:48:06 +0200

    Update ContactsBaseHelper.java

--- app/src/main/java/com/example/imokmessenger/ContactsBaseHelper.java
+++ app/src/main/java/com/example/imokmessenger/ContactsBaseHelper.java
@@ -7,15 +7,17 @@ public class ContactsBaseHelper extends SQLiteOpenHelper {
   //имя БД
   private static final String DATABASE_NAME = "contactsBase.db";
   
-  public CrimeBaseHelper(Context context) {
+  public ContactsBaseHelper(Context context) {
     super(context, DATABASE_NAME, null, VERSION);
   }
   
   //метод вызывается если БД еще не создана
   @Override
-  public void onCreate(SQLiteDatabase db) {}
+  public void onCreate(SQLiteDatabase db) {
+    db.execSQL(ContactsDbSchema.DB_CONTACTS_CREATE);
+  }
   
-  //метод вызывается если БД
+  //метод вызывается если БД обновляется
   @Override
   public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {}
   
