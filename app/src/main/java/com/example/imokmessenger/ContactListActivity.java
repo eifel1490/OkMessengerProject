2e766423a954e7c246a902c6860ddbfe11085771
eifel1390 <sds05031953>
2017-11-14 22:38:50 +0200

    1411

--- app/src/main/java/com/example/imokmessenger/ContactListActivity.java
+++ app/src/main/java/com/example/imokmessenger/ContactListActivity.java
@@ -11,6 +11,7 @@ import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.v4.content.LocalBroadcastManager;
@@ -24,6 +25,7 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.prefs.Preferences;
 
 /*класс отображающий список конактов для пользователя*/
@@ -50,74 +52,16 @@ public class ContactListActivity extends AppCompatActivity {
         btnConfirm.setEnabled(false);
         //инициируем БД-хелпер
         dbHelper = new ContactsBaseHelper(this);
-        getAllContacts();
-        adapterLauncher();
+        new MyTask().execute();
         
         //инициализируем LocalBroadcastManager для "отлова" сообщений из адаптера
         LocalBroadcastManager.getInstance(this).registerReceiver(mMessageReceiver,new IntentFilter("custom-message"));
     }
 
-    //метод выбирает контакты из курсора contentResolver-а и пишет их построчно в БД
-    private void getAllContacts() {
-        
-        //получаем доступ к базе
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
-        
-        // создаем объект для данных
-        ContentValues cv = new ContentValues();
-        
-        //создаем обьект contentResolver
-        ContentResolver contentResolver = getContentResolver();
-        //TODO попробовать другой алгоритм вытяжки данных из ContentProvider,этот долго работает
-        //или поставить этот алгоритм в фоновый режим
-        //создаем курсор с запросом "вытянуть все из contentProvider-базы ContactsContract.Contacts.CONTENT_URI"
-        Cursor cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC");
-        //если курсор вернулся непустой(то есть есть хотя бы одна строка)
-        if (cursor.getCount() > 0) {
-            //передвигаем парсер на первую позицию
-            while (cursor.moveToNext()) {
-                //получаем интовую переменную из поля contentProvider-базы,которая будет индикатором есть ли в контакте телефонный номер
-                int hasPhoneNumber = Integer.parseInt(cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)));
-                //если в контакте есть телефонный номер,то...
-                if (hasPhoneNumber > 0) {
-                    //...вычитываем значение ИД контакта
-                    String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
-                    //записываем его в contentValues
-                    cv.put("contact_id", id);
-                    //затем вычитываем значение имени контакта
-                    String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-                    //и тоже записываем его в contentValues
-                    cv.put("contact_name", name);
-                    //создаем курсор с запросом "вытянуть все из contentProvider-базы ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                    //где поле ContactsContract.CommonDataKinds.Phone.CONTACT_ID совпадает с введенным нами ИД
-                    Cursor phoneCursor = contentResolver.query(
-                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                            null,
-                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
-                            new String[]{id},
-                            null);
-                    //передвигаем парсер на первую позицию
-                    if (phoneCursor.moveToNext()) {
-                        //вычитываем значение телефонного номера контакта
-                        String phone = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-                        //записываем телефонный номер в contentValues
-                        cv.put("contact_phone", phone);
-                        //заполняем поле обьекта Контакт "телефонный номер"
-                        //userData.setContactNumber(phoneNumber);
-                    }
-                    //в колонку "выбрано" по умолчанию пишем 0,то есть не выбрано
-                    cv.put("selected", 0);
-                    //записываем строку в БД
-                    db.insert(ContactsDbSchema.ContactsTable.DB_TABLE, null, cv);
-                    //закрываем phoneCursor
-                    phoneCursor.close();
-                }
-            }
-        }
-    }
+
     
     //метод запускающий адаптер, на вход принимает список UserData
-    public void adapterLauncher(){
+    /*public void adapterLauncher(){
             List<UserData> list = getListFromDatabase();
             //создаем обьект адаптера,ему передается ArrayList и обьект контекста
             UserDataAdapter contactAdapter = new UserDataAdapter(list, getApplicationContext());
@@ -126,45 +70,9 @@ public class ContactListActivity extends AppCompatActivity {
             //присваиваем адаптер
             rvContacts.setAdapter(contactAdapter);
     
-    }
+    }*/
     
-    //метод формирующий список UserData из данных БД
-    public List<UserData> getListFromDatabase(){
-        //получаем доступ к БД
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
-        //создаем список UserData
-        List<UserData> userDataList = new ArrayList<>();
-        //создаем курсор читаем данные из него,пока они там есть (цикл)
-        Cursor c = db.query(ContactsDbSchema.ContactsTable.DB_TABLE, null, null, null, null, null, null);
-        //читаем данные из него,пока они там есть (цикл)
-            if (c.moveToFirst()) {
-                // определяем номера столбцов по имени в выборке
-                int idColIndex = c.getColumnIndex("contact_id");
-                int nameColIndex = c.getColumnIndex("contact_name");
-                int phoneColIndex = c.getColumnIndex("contact_phone");
-                int selectColIndex = c.getColumnIndex("selected");
-                
-                do {
-                  //создаем обьект типа UserData  
-                  UserData userData = new UserData();
-                  //Заполняем его поля значениями из БД  
-                  userData.setContactID(c.getString(idColIndex));
-                  userData.setContactName(c.getString(nameColIndex));
-                  userData.setContactNumber(c.getString(phoneColIndex));
-                  userData.setContactSelect(c.getString(selectColIndex));   
-                  //кладем в список
-                  userDataList.add(userData);  
-                  
-                  // переход на следующую строку ,а если следующей нет (текущая - последняя), то false - выходим из цикла
-                }
-                while (c.moveToNext());
-            } 
-            else Toast.makeText(this, "Ваша телефонная книга пуста!", Toast.LENGTH_SHORT).show();
-       c.close();
-       //возвращаем список с обьектами UserData
-        Log.d(TAG,"Количество контактов в ContactListActivity " + String.valueOf(userDataList.size()));
-       return userDataList; 
-    }
+
     
     
     //приемник локальных сообщений(транслируются только в нашем приложении)
@@ -183,17 +91,17 @@ public class ContactListActivity extends AppCompatActivity {
                 }
                 //если кнопка не нажата,то проверяем нажаты ли остальные кнопки
                 //и если ни одна не нажата,то кнопка Подтвердить неактивна
-                if(isListChecked()==false){
+                //if(isListChecked()==false){
 
-                    btnConfirm.setEnabled(false);
-                }
+                //    btnConfirm.setEnabled(false);
+                //}
             }
         }
     };
 
     //метод для проверки,есть ли нажатые кнопки.Возвращает false если ни одна кнопка не нажата
     //и true если нажата хотя бы одна кнопка
-    public boolean isListChecked(){
+    /*public boolean isListChecked(){
         //UserDataSingleton userDataSingleton = UserDataSingleton.getInstance();
         List<UserData>l = getListFromDatabase();
         boolean result = false;
@@ -205,9 +113,9 @@ public class ContactListActivity extends AppCompatActivity {
         }
         Log.d(TAG,"result "+ result);
         return result;
-    }
+    }*/
 
-    public void btnConfirmClick(View v) {
+    /*public void btnConfirmClick(View v) {
 
         if(isListChecked()==true) {
             ContactPreferences.setStoredQuery(this,"listWithChecked");
@@ -219,6 +127,107 @@ public class ContactListActivity extends AppCompatActivity {
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
         finish();
+    }*/
+
+    class MyTask extends AsyncTask<Void, Void, List<UserData>> {
+
+        @Override
+        protected List<UserData> doInBackground(Void... params) {
+            //получаем доступ к базе
+            SQLiteDatabase db = dbHelper.getWritableDatabase();
+            List<UserData> userDataList = new ArrayList<>();
+
+            // создаем объект для данных
+            ContentValues cv = new ContentValues();
+
+            //создаем обьект contentResolver
+            ContentResolver contentResolver = getContentResolver();
+            //TODO попробовать другой алгоритм вытяжки данных из ContentProvider,этот долго работает
+            //или поставить этот алгоритм в фоновый режим
+            //создаем курсор с запросом "вытянуть все из contentProvider-базы ContactsContract.Contacts.CONTENT_URI"
+            Cursor cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC");
+            //если курсор вернулся непустой(то есть есть хотя бы одна строка)
+            if (cursor.getCount() > 0) {
+                //передвигаем парсер на первую позицию
+                while (cursor.moveToNext()) {
+                    //получаем интовую переменную из поля contentProvider-базы,которая будет индикатором есть ли в контакте телефонный номер
+                    int hasPhoneNumber = Integer.parseInt(cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)));
+                    //если в контакте есть телефонный номер,то...
+                    if (hasPhoneNumber > 0) {
+                        //...вычитываем значение ИД контакта
+                        String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+                        //записываем его в contentValues
+                        cv.put("contact_id", id);
+                        //затем вычитываем значение имени контакта
+                        String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                        //и тоже записываем его в contentValues
+                        cv.put("contact_name", name);
+                        //создаем курсор с запросом "вытянуть все из contentProvider-базы ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                        //где поле ContactsContract.CommonDataKinds.Phone.CONTACT_ID совпадает с введенным нами ИД
+                        Cursor phoneCursor = contentResolver.query(
+                                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                                null,
+                                ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                                new String[]{id},
+                                null);
+                        //передвигаем парсер на первую позицию
+                        if (phoneCursor.moveToNext()) {
+                            //вычитываем значение телефонного номера контакта
+                            String phone = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                            //записываем телефонный номер в contentValues
+                            cv.put("contact_phone", phone);
+                            //заполняем поле обьекта Контакт "телефонный номер"
+                            //userData.setContactNumber(phoneNumber);
+                        }
+                        //в колонку "выбрано" по умолчанию пишем 0,то есть не выбрано
+                        cv.put("selected", 0);
+                        //записываем строку в БД
+                        db.insert(ContactsDbSchema.ContactsTable.DB_TABLE, null, cv);
+                        //закрываем phoneCursor
+                        phoneCursor.close();
+                    }
+                }
+            }
+            Cursor c = db.query(ContactsDbSchema.ContactsTable.DB_TABLE, null, null, null, null, null, null);
+            //читаем данные из него,пока они там есть (цикл)
+            if (c.moveToFirst()) {
+                // определяем номера столбцов по имени в выборке
+                int idColIndex = c.getColumnIndex("contact_id");
+                int nameColIndex = c.getColumnIndex("contact_name");
+                int phoneColIndex = c.getColumnIndex("contact_phone");
+                int selectColIndex = c.getColumnIndex("selected");
+
+                do {
+                    //создаем обьект типа UserData
+                    UserData userData = new UserData();
+                    //Заполняем его поля значениями из БД
+                    userData.setContactID(c.getString(idColIndex));
+                    userData.setContactName(c.getString(nameColIndex));
+                    userData.setContactNumber(c.getString(phoneColIndex));
+                    userData.setContactSelect(c.getString(selectColIndex));
+                    //кладем в список
+                    userDataList.add(userData);
+
+                    // переход на следующую строку ,а если следующей нет (текущая - последняя), то false - выходим из цикла
+                }
+                while (c.moveToNext());
+            }
+            c.close();
+            //возвращаем список с обьектами UserData
+            Log.d(TAG,"Количество контактов в ContactListActivity " + String.valueOf(userDataList.size()));
+            return userDataList;
+
+        }
+
+        @Override
+        protected void onPostExecute(List<UserData>list) {
+            super.onPostExecute(list);
+            UserDataAdapter contactAdapter = new UserDataAdapter(list, getApplicationContext());
+            //расположение будет вертикальным списком
+            rvContacts.setLayoutManager(new LinearLayoutManager(getBaseContext()));
+            //присваиваем адаптер
+            rvContacts.setAdapter(contactAdapter);
+        }
     }
 
 
