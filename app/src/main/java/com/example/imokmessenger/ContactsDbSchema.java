05142e9de22309d3a6928a3724fcc705e9369284
eifel1390 <maksimturkin50@gmail.com>
2017-11-15 12:49:23 +0200

    Update ContactsDbSchema.java

--- app/src/main/java/com/example/imokmessenger/ContactsDbSchema.java
+++ app/src/main/java/com/example/imokmessenger/ContactsDbSchema.java
@@ -6,17 +6,17 @@ public class ContactsDbSchema {
     //название таблицы
     public static final String DB_TABLE = "contacts";
     
-    public static final class Cols {
-      //столбец ИД
-      public static final String ID = "contact_id";
-      //столбец имя контакта
-      public static final String NAME = "contact_name";
-      //столбец номер контакта
-      public static final String PHONE = "contact_phone";
-      //столбец выбран ли контакт
-      public static final String SELECTED = "selected";
+      public static final class Cols {
+        //столбец ИД
+        public static final String ID = "contact_id";
+        //столбец имя контакта
+        public static final String NAME = "contact_name";
+        //столбец номер контакта
+        public static final String PHONE = "contact_phone";
+        //столбец выбран ли контакт
+        public static final String SELECTED = "selected";
+      }
     }
-  
   }
   
   //команда создания БД
