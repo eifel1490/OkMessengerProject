8ea82f2f7cb9290af87d2b4c7c7d2cf5d5f55497
eifel1390 <sds05031953>
2017-11-16 22:28:42 +0200

    1611.2

--- app/src/main/java/com/example/imokmessenger/ContactsMessageActivity.java
+++ app/src/main/java/com/example/imokmessenger/ContactsMessageActivity.java
@@ -2,17 +2,46 @@ package com.example.imokmessenger;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
- * Created by Максим on 16.11.2017.
+ *
  */
 
 public class ContactsMessageActivity extends Activity {
+
+    EditText editUserText;
+    Button cancelMessage, saveMessage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.contacts_message_activity);
+
+        editUserText = (EditText) findViewById(R.id.editText_message);
+        cancelMessage = (Button) findViewById(R.id.button_cancel);
+        saveMessage = (Button) findViewById(R.id.button_save);
+
     }
+
+    void onClickSaveButton(View v){
+        if (editUserText.getText().length() == 0){
+            return;
+        }
+        else {
+            saveTextToDB(editUserText.getText().toString());
+        }
+    }
+
+    void saveTextToDB(String s){
+        ContactsBaseHelper dbHelper;
+    }
+
+
+
+
 
 }
