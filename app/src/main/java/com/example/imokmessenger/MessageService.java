04f655a105edbdc5c700af5234a9f9725fa083d2
eifel1390 <maksimturkin50@gmail.com>
2017-12-13 16:56:41 +0200

    Update MessageService.java

--- app/src/main/java/com/example/imokmessenger/MessageService.java
+++ app/src/main/java/com/example/imokmessenger/MessageService.java
@@ -3,12 +3,17 @@
 /*сервис.Будет работать в фоне*/
 
 public class MessageService extends IntentService {
+   
+   public MessageService() {
+    super("messageService");
+  }
 
    @Override
     protected void onHandleIntent(Intent intent) {
         //,прослушивать BroadcastReceiver и при достижении
         //уровня аккумулятора меньше Х процентов выполнять действие
         //здесь выполняется код из ManageMessage
+       
     }
 
 
