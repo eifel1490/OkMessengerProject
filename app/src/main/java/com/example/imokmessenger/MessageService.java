bfa6fa90ce16a3f34f85f1ca0fbae6d0c834b18b
eifel1390 <maksimturkin50@gmail.com>
2017-12-13 14:59:54 +0200

    Create MessageService.java

---
+++ app/src/main/java/com/example/imokmessenger/MessageService.java
@@ -0,0 +1,10 @@
+
+
+/*сервис.Будет работать в фоне,прослушивать BroadcastReceiver и при достижении
+уровня аккумулятора меньше Х процентов выполнять действие*/
+
+public class MessageService extends IntentService {
+
+
+
+}
