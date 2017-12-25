6b5c93b71829380b06f92f7745b6038bff81010e
eifel1390 <maksimturkin50@gmail.com>
2017-12-25 10:07:00 +0200

    Update ContactPreferences.java

--- app/src/main/java/com/example/imokmessenger/ContactPreferences.java
+++ app/src/main/java/com/example/imokmessenger/ContactPreferences.java
@@ -3,12 +3,14 @@ package com.example.imokmessenger;
 import android.content.Context;
 import android.preference.PreferenceManager;
 
+import static android.R.id.message;
 
 
 public class ContactPreferences {
 
     //константа,было ли создано сообщение
     private static final String STORED_MESSAGE = "stored_message";
+    private static final String STORED_CHARGE = "stored_message";
 
     //метод,возвращающий индикатор,было ли создано сообщение
     public static String getStoredMessage(Context context){
@@ -23,5 +25,21 @@ public class ContactPreferences {
                 .putString(STORED_MESSAGE,message)
                 .apply();
     }
+
+    public static String getStoredCharge(Context context){
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getString(STORED_CHARGE,null);
+    }
+
+    //записывает идентификатор создания сообщения
+    public static void setStoredCharge(Context context,String message) {
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit()
+                .putString(STORED_CHARGE,message)
+                .apply();
+    }
+
+
+
 
 }
