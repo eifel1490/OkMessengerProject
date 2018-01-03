4e26f4f2d62366e3716154ad3fb60638496e7c46
eifel1390 <maksimturkin50@gmail.com>
2018-01-03 11:00:17 +0200

    Create MainApplication.java

---
+++ app/src/main/java/com/example/imokmessenger/MainApplication.java
@@ -0,0 +1,13 @@
+package com.example.imokmessenger;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.example.imokmessenger.helper.LocaleHelper;
+
+public class MainApplication extends Application {
+	@Override
+	protected void attachBaseContext(Context base) {
+		super.attachBaseContext(LocaleHelper.onAttach(base, "en"));
+	}
+}
