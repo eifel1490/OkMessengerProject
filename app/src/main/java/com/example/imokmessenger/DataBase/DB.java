a53b4a8fd9f5e494e203573354efe98ba13faefa
eifel1390 <maksimturkin50@gmail.com>
2018-02-09 12:44:52 +0200

    Update DB.java

--- app/src/main/java/com/example/imokmessenger/DataBase/DB.java
+++ app/src/main/java/com/example/imokmessenger/DataBase/DB.java
@@ -24,71 +24,41 @@ public class DB {
     private final Context mCtx;
     private DBHelper mDBHelper;
     private SQLiteDatabase mDB;
-
-    public static final class ContactsTable {
-        
-        public static final String DB_TABLE = "contacts";
-
-        public static final class Cols {
-            
-            public static final String ID = "contact_id";
-            public static final String NAME = "contact_name";
-            public static final String PHONE = "contact_phone";
-            public static final String SELECTED = "selected";
-        }
-    }
-
     
-    public static final String DB_CONTACTS_CREATE =
-            "create table " + ContactsTable.DB_TABLE + "(" + " _id integer primary key autoincrement, " +
-                    ContactsTable.Cols.ID + ", " +
-                    ContactsTable.Cols.NAME + ", " +
-                    ContactsTable.Cols.PHONE + ", " +
-                    ContactsTable.Cols.SELECTED +
-                    ")";
-
-    
-
     public DB(Context ctx) {
-        mCtx = ctx;
+            mCtx = ctx;
     }
-
-    // открыть подключение
+    
     public void open() {
-        mDBHelper = new DBHelper(mCtx);
-        mDB = mDBHelper.getWritableDatabase();
+            mDBHelper = new DBHelper(mCtx);
+            mDB = mDBHelper.getWritableDatabase();
     }
-
-    // закрыть подключение
+    
     public void close() {
         if (mDBHelper!=null) mDBHelper.close();
     }
-
-    // получить все данные из таблицы DB_TABLE
+    
     public Cursor getAllData() {
         return mDB.query(ContactsTable.DB_TABLE, null, null, null, null, null, null);
     }
-
-    //метод для проверки,есть ли нажатые кнопки.Возвращает false если ни одна кнопка не нажата
-    //и true если нажата хотя бы одна кнопка
+    
+    //Method to check if there are any buttons pressed. Returns false if no button is pressed
+    // and true if at least one button is pressed
     public boolean isListChecked(){
-        //результат по умолчанию false
+        
         boolean result = false;
-        //открываем доступ к БД
         open();
-        //создаем курсор,в который идет выкачка инфо методом query.Query вытягивает из таблицы только те значения в столце "Выбрано"
-        //которые равны 1
+        
         Cursor cursor = mDB.query(ContactsDbSchema.ContactsTable.DB_TABLE,
                 new String[]{ContactsDbSchema.ContactsTable.Cols.SELECTED},"selected = ?",new String[]{"1"},null,null,null);
-        //если курсор непустой,то значит есть минимум одно значение равное 1,тогде ставим результат true
+        
         if(cursor.getCount()>0) result = true;
-        //закрываем курсор
         cursor.close();
-        //возвращаем результат
+        
         return result;
     }
-
-    //возвращает список чекнутых контактов для инфо на главной странице
+    
+    //returns a list of checked contacts for info on the main page
     public List<String> getCheckedContacts(){
         List<String>list= new ArrayList<>();
         Cursor c = mDB.query(ContactsDbSchema.ContactsTable.DB_TABLE,
@@ -97,7 +67,6 @@ public class DB {
             try {
 
                 if (c.moveToFirst()) {
-                    // определяем номера столбцов по имени в выборке
                     int nameColIndex = c.getColumnIndex("contact_name");
                     do {
                         Log.d(TAG, c.getString(nameColIndex));
@@ -114,29 +83,50 @@ public class DB {
         }
         return list;
     }
-
-    //вставка в БД
+    
+    
     public void insert(ContentValues cv){
         mDB.insert(ContactsDbSchema.ContactsTable.DB_TABLE, null, cv);
     }
 
-    //очистка БД
+    
     public void deleteAllData(){
         mDB.delete(ContactsDbSchema.ContactsTable.DB_TABLE, null, null);
     }
 
-    //обновление БД
+    
     public void updateData(ContentValues cv,String contact_Id){
         mDB.update(ContactsDbSchema.ContactsTable.DB_TABLE , cv, ContactsDbSchema.ContactsTable.Cols.ID + " = ?",new String[] { contact_Id });
     }
 
+    //returns all records where "selected" equals 1
     public Cursor queryData(){
         return mDB.query(ContactsDbSchema.ContactsTable.DB_TABLE,
                 null,"selected = ?",new String[]{"1"},null,null,null);
     }
+    
+    public static final String DB_CONTACTS_CREATE =
+            "create table " + ContactsTable.DB_TABLE + "(" + " _id integer primary key autoincrement, " +
+                    ContactsTable.Cols.ID + ", " +
+                    ContactsTable.Cols.NAME + ", " +
+                    ContactsTable.Cols.PHONE + ", " +
+                    ContactsTable.Cols.SELECTED +
+                    ")";
 
+    
 
+    public static final class ContactsTable {
+        
+        public static final String DB_TABLE = "contacts";
 
+        public static final class Cols {
+            
+            public static final String ID = "contact_id";
+            public static final String NAME = "contact_name";
+            public static final String PHONE = "contact_phone";
+            public static final String SELECTED = "selected";
+        }
+    }
 
     public class DBHelper extends SQLiteOpenHelper {
 
@@ -144,18 +134,14 @@ public class DB {
             super(context, DATABASE_NAME, null, VERSION);
         }
 
-        //метод вызывается если БД еще не создана
         @Override
         public void onCreate(SQLiteDatabase db) {
             db.execSQL(ContactsDbSchema.DB_CONTACTS_CREATE);
         }
 
-        //метод вызывается если БД обновляется
         @Override
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {}
 
     }
-
-
 }
 
